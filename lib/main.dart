33d609911ab39a40949a34ec6f283530f4dc4d3f
Wayne <waynevick7226@hotmail.com>
2020-02-25 10:58:49 -0500

    second commit, adding files

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+void main() => runApp(MyApp());
+
+@override
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex < 4) _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite meal?',
+      'What\'s your favorite brand?',
+      'End of Questions'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(questions.elementAt(_questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: _answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
