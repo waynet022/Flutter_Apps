86a73b060a393c232cb502235fe15a0c063fe9d8
Wayne <waynevick7226@hotmail.com>
2020-02-25 16:40:12 -0500

    separated widgets from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +13,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'what\'s your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -41,7 +41,7 @@ class _MyAppState extends State<MyApp> {
     );
     print(_questionIndex);
 
-    if(_questionIndex < questions.length){
+    if(_questionIndex < _questions.length){
       print('We have more questions');
     }
     else{
@@ -57,20 +57,14 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < questions.length
-            ?Column(
-              children: [
-                Question(
-                  questions[_questionIndex]['questionText'],
-                ),
-                ...(questions[_questionIndex]['answers'] as List<String>)
-                  .map((answer) {
-                  return Answer(_answerQuestion, answer);
-                }).toList()
-            ],
-          ) : Center(child: Text('you did it'),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions:_questions
+              )
+            : Result(),
           ),
-      ),
-    );
+      );
   }
 }
---
+++ lib/result.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@override
+class Result extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text('you did it'),
+    );
+  }
+}
