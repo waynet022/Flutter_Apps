b09e69c8d8f2c060a8c4a5adb92ed776659e8d15
waynet022 <46969984+waynet022@users.noreply.github.com>
2020-03-18 21:28:14 -0400

    Setting new home page
    Updated the categories_screen with the different categories into the
    grid.
    
    Setup the new home page and deleted the previous default home page.

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,13 +5,17 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
         children: DUMMY_CATEGORIES
             .map(
                 (catData) => CategoryItem(
-                    catData.title,
-                    catData.color,
-                )
+              catData.title,
+              catData.color,
+              )
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
@@ -19,7 +23,8 @@ class CategoriesScreen extends StatelessWidget {
           childAspectRatio: 3/2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
-      ),
+        ),
+      )
     );
   }
 }
